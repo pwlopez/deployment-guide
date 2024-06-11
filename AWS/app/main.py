50b94e10558410579b2b767e693b0e64a6d2efb3
Parker <parkerlopez17@gmail.com>
2024-06-11 09:33:22 -0700

    baseline model methods addded

--- AWS/app/main.py
+++ AWS/app/main.py
@@ -1,25 +1,52 @@
+import json
+import joblib
+
 # This contains all the necessary functions for serving the custom model
 
-def model_fn():
+def model_fn(model_dir):
     """
-    Load the model
+    Load the model using the preferred method. In this sample case we use joblib 
+    which can handle a wide variety of model types. When possible, use the recommended
+    serialization tools for your specific model. As a result, this function may be many
+    lines longer.
+
+    model_dir: model can be packed in the container alongside this code for deployment. 
+    Make sure the model is stored in the correct location. In this case /app/
     """
-    pass
+    model = joblib.load(model_dir)
+
+    return model
 
 def input_fn(request, content_type):
     """
-    Accept parameters passed from Sagemaker
+    Accept the request and request format (ex. json, text, etc)
     """
-    pass
+
+    # If there are multiple request options, make sure you verify the format
+    if  content_type == "application/json":
+        body = json.loads(request)
+
+    # Perform any necessary data transformations to prepare for model ingestion
+
+    return body
 
 def predict_fn(input_data, model):
     """
     Perform model inference
     """
-    pass
+    
+    output = model.predict(input_data)
 
-def output_fn(data, content_type):
+    return output
+
+def output_fn(output, accept):
     """
     Build output object and return
+
+    Assume the accept content type is the same as input: application/json, so
+    we build a json object and return it
     """
-    pass
+    
+    output = json.dumps(output)
+
+    return output, accept
